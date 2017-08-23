80ba627f6594468cd75c2802d5afe6e04af1fa52
Gergő Móricz <mo.geryy@gmail.com>
2017-08-23 14:45:53 +0200

    Added folder specifying

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
-// Require libraries
+//Require libraries
 const chalk = require('chalk'); // Colored logging
 var fs = require('fs'); // Filesystem
+
+var folder = process.argv[2] || __dirname; // Specified folder (argument or current working directory)
